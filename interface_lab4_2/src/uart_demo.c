c137621c738ee0b7610460111981d8014a2bf657
MiRaCLeXeoN <zzp905020708@gmail.com>
2023-11-22 17:14:48 +0800

    lab4_2 src updated

--- interface_lab4_2/src/uart_demo.c
+++ interface_lab4_2/src/uart_demo.c
@@ -1,24 +1,44 @@
-#if defined(D_NEXYS_A7)
-   #include <bsp_printf.h>
-   #include <bsp_mem_map.h>
-   #include <bsp_version.h>
-#else
-   PRE_COMPILED_MSG("no platform was defined")
-#endif
-#include <psp_api.h>
+#define UART_BASE 0x80111000
+#define rbr     0*4
+#define ier     1*4
+#define fcr     2*4
+#define lcr     3*4
+#define mcr     4*4
+#define lsr     5*4
+#define msr     6*4
+#define scr     7*4
 
-int main(void)
-{
-   int i;
+#define thr     rbr
+#define iir     fcr
+#define dll     rbr
+#define dlm     ier
 
-   /* Initialize Uart */
-   uartInit();
+#define WRITE_UART(dir, value) { (*(volatile unsigned *)(dir)) = (value); }
+int main() {
+    volatile unsigned int i, j;
+    char res[20] = "hello world\n";
 
-   while(1){
-      /* Print "hello world" message on the serial output (be carrefoul not all the printf formats are supported) */
-      printfNexys("hello world\n");
-      /* Delay */
-      for (i=0;i<10000000;i++){};
-   }
+    WRITE_UART(UART_BASE+lcr, 0x00000080);
+    for(j = 0; j < 1000000; j++) ;
+    WRITE_UART(UART_BASE+dll, 27);
+    for(j = 0; j < 1000000; j++) ;
+    WRITE_UART(UART_BASE+fcr, 0x0);
+    for(j = 0; j < 1000000; j++) ;
+    WRITE_UART(UART_BASE+dlm, 0x00000000);
+    for(j = 0; j < 1000000; j++) ;
+    WRITE_UART(UART_BASE+lcr, 0x00000003);
+    for(j = 0; j < 1000000; j++) ;
+    WRITE_UART(UART_BASE+ier, 0x00000000);
+    for(j = 0; j < 10000000; j++) ;
 
+    while (1)
+    {
+        for(i = 0; res[i] != '\0'; i++) {
+            WRITE_UART(UART_BASE + thr, res[i]);
+            for(j = 0; j < 100000; j++) 
+                ;
+        }
+    }
+
+    return 0;    
 }
